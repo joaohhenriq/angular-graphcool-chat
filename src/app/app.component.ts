3e3c1c3dba54d91da6f48ffb7090eda88071516a
joaohhenriq <joao_camargo10@hotmail.com>
2018-10-24 09:28:38 -0300

    Utilizando graphql com HttpCliente, trabalhando com mutation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,7 @@ import { HttpClient } from '@angular/common/http';
 export class AppComponent {
   private apiUrl = 'https://api.graph.cool/simple/v1/cjniussik5sar0177vc58th1h';
   constructor(private http: HttpClient) {
+    this.createUser();
     this.allUser();
   }
 
@@ -27,5 +28,31 @@ export class AppComponent {
 
     this.http.post(this.apiUrl, body)
       .subscribe(res => console.log('Query: ', res));
+  }
+
+  createUser(): void {
+    const body = {
+      query: `
+        mutation CreateNewUser(
+          $name: String!,
+          $email: String!,
+          $password: String!
+        ) {
+          createUser(name: $name, email: $email, password: $password) {
+            id
+            name
+            email
+          }
+        }
+      `,
+      variables: {
+        name: 'teste',
+        email: 'teste@teste.com',
+        password: '123456'
+      }
+    };
+
+    this.http.post(this.apiUrl, body)
+      .subscribe(res => console.log('Mutation: ', res));
   }
 }
