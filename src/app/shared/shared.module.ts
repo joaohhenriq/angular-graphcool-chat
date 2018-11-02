ced44efff061a2f6d18ff88e0f7ce771e2d05ef6
joaohhenriq <joao_camargo10@hotmail.com>
2018-11-02 14:07:35 -0300

    Desenhando tela do login

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatCardModule, MatToolbarModule, MatFormFieldModule, MatInputModule, MatButtonModule } from '@angular/material';
 
 @NgModule({
   exports: [
-    CommonModule
+    CommonModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule
   ]
 })
 export class SharedModule { }
