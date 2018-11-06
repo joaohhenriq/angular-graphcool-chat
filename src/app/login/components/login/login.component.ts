dffed58fce39a40236b5a132cf348ccabe5035cc
joaohhenriq <joao_camargo10@hotmail.com>
2018-11-05 22:39:06 -0200

    Utilizando sign in e sign up no mesmo componente

--- src/app/login/components/login/login.component.ts
+++ src/app/login/components/login/login.component.ts
@@ -9,6 +9,12 @@ import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms'
 export class LoginComponent implements OnInit {
 
   loginForm: FormGroup;
+  configs = {
+    isLogin: true,
+    actionText: 'Sign In',
+    buttonActionText: 'Create account'
+  };
+  private nameControl = new FormControl('', [Validators.required, Validators.minLength(5)]);
 
   constructor(private formBuilder: FormBuilder) { }
 
@@ -27,6 +33,15 @@ export class LoginComponent implements OnInit {
     console.log(this.loginForm.value);
   }
 
+  changeAction(): void {
+    this.configs.isLogin = !this.configs.isLogin;
+    this.configs.actionText = !this.configs.isLogin ? 'Sign Up' : 'Sign In';
+    this.configs.buttonActionText = !this.configs.isLogin ? 'Already have account' : 'Create account';
+
+    !this.configs.isLogin ? this.loginForm.addControl('name', this.nameControl) : this.loginForm.removeControl('name');
+  }
+
+  get name(): FormControl { return <FormControl>this.loginForm.get('name'); }
   get email(): FormControl { return <FormControl>this.loginForm.get('email'); }
   get password(): FormControl { return <FormControl>this.loginForm.get('password'); }
 
