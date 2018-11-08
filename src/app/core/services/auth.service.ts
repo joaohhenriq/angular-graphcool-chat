a2363d4d9be5e6d64452f4a5440380a38a050e1c
joaohhenriq <joao_camargo10@hotmail.com>
2018-11-08 19:42:12 -0200

    Criando o método de signup da aplicação

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,4 +1,4 @@
-import { AUTHENTICATE_USER_MUTATION } from './auth.graphql';
+import { AUTHENTICATE_USER_MUTATION, SIGNUP_USER_MUTATION } from './auth.graphql';
 import { Apollo } from 'apollo-angular';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
@@ -12,8 +12,8 @@ export class AuthService {
   constructor(
     private apollo: Apollo
   ) {
-    this.signinUser({email: 'angular@pop.com', password: '123456'})
-      .subscribe(res => console.log('SignInUser', res));
+    this.signupUser({name: 'juao', email: 'juao@hotmail.com', password: '123456'})
+      .subscribe(res => console.log('SignUpUser', res));
   }
 
   signinUser(variables: {email: string, password: string}): Observable<{id: string, token: string}> {
@@ -22,6 +22,15 @@ export class AuthService {
       variables: variables
     }).pipe(
       map(res => res.data.authenticateUser)
+    );
+  }
+
+  signupUser(variables: {name: string, email: string, password: string}): Observable<{id: string, token: string}> {
+    return this.apollo.mutate({
+      mutation: SIGNUP_USER_MUTATION,
+      variables: variables
+    }).pipe(
+      map(res => res.data.signupUser)
     );
   }
 }
