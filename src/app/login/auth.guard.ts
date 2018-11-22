b224129a9aafac6cf8119346f22af76b4f57a556
joaohhenriq <joao_camargo10@hotmail.com>
2018-11-22 20:22:57 -0200

    Redirecionamento para o login quando usuário acessar tela que requer autenticação

--- src/app/login/auth.guard.ts
+++ src/app/login/auth.guard.ts
@@ -2,17 +2,21 @@ import { Observable } from 'rxjs';
 import { AuthService } from './../core/services/auth.service';
 import { LoginRoutingModule } from './login-routing.module';
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, CanActivateChild, CanLoad, Route } from '@angular/router';
-import { take } from 'rxjs/operators';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, CanActivateChild, CanLoad, Route, Router } from '@angular/router';
+import { take, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: LoginRoutingModule
 })
 export class AuthGuard implements CanActivate, CanActivateChild, CanLoad {
-  constructor(private authService: AuthService) { }
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
-    return this.authService.isAuthenticated;
+    return this.checkAuthState();
   }
 
   canActivateChild(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
@@ -20,9 +24,20 @@ export class AuthGuard implements CanActivate, CanActivateChild, CanLoad {
   }
 
   canLoad(route: Route): Observable<boolean> {
-    return this.authService.isAuthenticated
+    return this.checkAuthState()
       .pipe(
         take(1) // pega somente o primeiro valor emitido
+      );
+  }
+
+  private checkAuthState(): Observable<boolean> {
+    return this.authService.isAuthenticated
+      .pipe(
+        tap(is => {
+          if (!is) {
+            this.router.navigate(['/login']);
+          }
+        })
       );
   }
 }
