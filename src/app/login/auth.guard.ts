8d85d61fc76761b51d1e9c6d7299c2c012b1a31c
joaohhenriq <joao_camargo10@hotmail.com>
2018-11-21 23:44:16 -0200

    Implementando CanActivateChild no AuthGuard

--- src/app/login/auth.guard.ts
+++ src/app/login/auth.guard.ts
@@ -2,15 +2,19 @@ import { Observable } from 'rxjs';
 import { AuthService } from './../core/services/auth.service';
 import { LoginRoutingModule } from './login-routing.module';
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, CanActivateChild } from '@angular/router';
 
 @Injectable({
   providedIn: LoginRoutingModule
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate, CanActivateChild {
   constructor(private authService: AuthService) { }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
     return this.authService.isAuthenticated;
+  }
+
+  canActivateChild(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
+    throw new Error("Method not implemented.");
   }
 }
