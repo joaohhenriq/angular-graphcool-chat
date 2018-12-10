c4be7869cc9734583f97dfc82db093cfd475f717
joaohhenriq <joao_camargo10@hotmail.com>
2018-12-10 20:45:18 -0200

    Carregando o modulo do chat com lazy loading

--- src/app/chat/chat-routing.module.ts
+++ src/app/chat/chat-routing.module.ts
@@ -1,7 +1,15 @@
+import { AuthGuard } from './../login/auth.guard';
+import { ChatTabComponent } from './components/chat-tab/chat-tab.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ChatTabComponent,
+    canActivate: [AuthGuard]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
