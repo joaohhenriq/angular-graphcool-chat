ac32a57099f33c125683dda9f9d19c957e1fb5dd
joaohhenriq <joao_camargo10@hotmail.com>
2018-12-11 20:19:47 -0200

    Criando método para buscar as informações dos usuários do graphcool

--- src/app/core/services/user.service.ts
+++ src/app/core/services/user.service.ts
@@ -1,9 +1,25 @@
+import { ALL_USERS_QUERY, AllUsersQuery } from './user.graphql';
+import { Apollo } from 'apollo-angular';
 import { Injectable } from '@angular/core';
+import { User } from '../models/user.model';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  constructor(
+    private apollo: Apollo
+  ) { }
+
+  allUsers(): Observable<User[]> {
+    return this.apollo
+      .query<AllUsersQuery>({
+        query: ALL_USERS_QUERY
+      }).pipe(
+        map(res => res.data.allUsers)
+      );
+  }
 }
