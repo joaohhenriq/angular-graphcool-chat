a5b888887a219c347938292177a7e454e5b82a45
joaohhenriq <joao_camargo10@hotmail.com>
2018-12-20 21:35:48 -0200

    Acessando Cache diretamente após uma mutation e reescrevendo a Query

--- src/app/chat/services/message.service.ts
+++ src/app/chat/services/message.service.ts
@@ -1,9 +1,11 @@
+import { variable } from '@angular/compiler/src/output/output_ast';
 import { AllMessagesQuery, GET_CHAT_MESSAGES_QUERY, CREATE_MESSAGE_MUTATION } from './message.graphql';
 import { Apollo } from 'apollo-angular';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Message } from '../models/message.model';
 import { map } from 'rxjs/operators';
+import { DataProxy } from 'apollo-cache';
 
 @Injectable({
   providedIn: 'root'
@@ -15,18 +17,34 @@ export class MessageService {
   ) { }
 
   getChatMessages(chatId: string): Observable<Message[]> {
-    return this.apollo.query<AllMessagesQuery>({
+    return this.apollo.watchQuery<AllMessagesQuery>({
       query: GET_CHAT_MESSAGES_QUERY,
       variables: { chatId }
-    }).pipe(
-      map(res => res.data.allMessages)
-    );
+    }).valueChanges
+      .pipe(
+        map(res => res.data.allMessages)
+      );
   }
 
   createMessage(message: {text: string, chatId: string, senderId: string}): Observable<Message> {
     return this.apollo.mutate({
       mutation: CREATE_MESSAGE_MUTATION,
-      variables: message
+      variables: message,
+      update: (store: DataProxy, {data: { createMessage }}) => {
+        const data = store.readQuery<AllMessagesQuery>({
+          query: GET_CHAT_MESSAGES_QUERY,
+          variables: { chatId: message.chatId }
+        });
+
+        // recurso spread, espalha os itens dos array em cada posição do novo array
+        data.allMessages = [...data.allMessages, createMessage]; // createMessage contém as propriedades da nova mensagem
+
+        store.writeQuery({
+          query: GET_CHAT_MESSAGES_QUERY,
+          variables: { chatId: message.chatId },
+          data: data
+        });
+      }
     }).pipe(
       map(res => res.data.createMessage)
     );
